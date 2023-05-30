22ecd4eed3da362b3bccbd415976c0c35d4a57fc
galemwendar <galemwendar@gmail.com>
2023-05-30 18:38:41 +0300

    Initial

--- centrvd.StudySolution/centrvd.StudySolution.Server/DocumentRegister/DocumentRegisterServerFunctions.cs
+++ centrvd.StudySolution/centrvd.StudySolution.Server/DocumentRegister/DocumentRegisterServerFunctions.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using Sungero.Core;
@@ -9,6 +9,11 @@ namespace centrvd.StudySolution.Server
 {
   partial class DocumentRegisterFunctions
   {
-
+    public override string GetNextNumber(DateTime date, int leadDocumentId, Sungero.Docflow.IOfficialDocument document, string leadingDocumentNumber, int departmentId, int businessUnitId, string caseFileIndex, string docKindCode, string indexLeadingSymbol)
+    {
+      //добавить ID НОР
+      return base.GetNextNumber(date, leadDocumentId, document, leadingDocumentNumber, departmentId, businessUnitId, caseFileIndex, docKindCode, indexLeadingSymbol);
+    }
+    
   }
 }
--- centrvd.StudySolution/centrvd.StudySolution.Shared/DocumentRegister/DocumentRegisterStructures.cs
+++ centrvd.StudySolution/centrvd.StudySolution.Shared/DocumentRegister/DocumentRegisterStructures.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using Sungero.Core;
@@ -6,5 +6,25 @@ using Sungero.CoreEntities;
 
 namespace centrvd.StudySolution.Structures.Docflow.DocumentRegister
 {
+  /// <summary>
+  /// Индекс регистрационного номера.
+  /// </summary>
+  partial class RegistrationNumberIndex
+  {
+    public int Index { get; set; }
+    
+    public string Postfix { get; set; }
+    
+    public string CorrectingPostfix { get; set; }
+  }
 
+  /// <summary>
+  /// Префикс и постфикс регистрационного номера документа.
+  /// </summary>
+  partial class RegistrationNumberParts
+  {
+    public string Prefix { get; set; }
+    
+    public string Postfix { get; set; }
+  }
 }
