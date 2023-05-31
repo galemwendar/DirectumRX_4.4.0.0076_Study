3e02d76547dbd97fe06545e1d22b7c39ab8123cd
galemwendar <galemwendar@gmail.com>
2023-05-31 08:53:41 +0300

    промежуточный

--- centrvd.StudySolution/centrvd.StudySolution.Server/OfficialDocument/OfficialDocumentHandlers.cs
+++ centrvd.StudySolution/centrvd.StudySolution.Server/OfficialDocument/OfficialDocumentHandlers.cs
@@ -12,7 +12,6 @@ namespace centrvd.StudySolution
 
     public override IQueryable<T> DocumentKindSearchDialogFiltering(IQueryable<T> query, Sungero.Domain.PropertySearchDialogFilteringEventArgs e)
     {
-
       query = base.DocumentKindSearchDialogFiltering(query, e);
       return query;
     }
@@ -22,13 +21,8 @@ namespace centrvd.StudySolution
   {
 
     public override IQueryable<T> DocumentKindFiltering(IQueryable<T> query, Sungero.Domain.PropertyFilteringEventArgs e)
-    {
+    {   
       //TODO Да еперный театр, я не хочу просто возвращать нуль если нет НОР
-      var list = new List<IRecipient>();
-      foreach (var element in query)
-      {
-        list.AddRange(element.AccessRights.GetSubstitutedWhoCanSelectInDocument());
-      }
       query = base.DocumentKindFiltering(query, e);
       return query;
     }
